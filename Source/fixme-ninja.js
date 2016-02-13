f3027757456094c064213ee4cf8016295fa0e5c6
ELLIOTTCABLE <me@ell.io>
2016-02-12 19:11:00 -0600

    + await!

--- Source/fixme-ninja.js
+++ Source/fixme-ninja.js
@@ -1,5 +1,9 @@
 import Git from "nodegit"
+import Debug from "debug"
+const debug = Debug('fixme-ninja')
 
-export default function ninja(repo){
-   Git.Repository.open(repo)
+export default async function ninja(path){
+   let repo = await Git.Repository.open(path)
+   let HEAD = await repo.getHeadCommit()
+   debug(`HEAD: ${HEAD.id().toString()}`)
 }
