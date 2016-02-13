005f5738ecba27b603a88f2466225050d593c1a2
ELLIOTTCABLE <me@ell.io>
2016-02-12 22:06:21 -0600

    + wow such crash

--- Source/fixme-ninja.js
+++ Source/fixme-ninja.js
@@ -1,13 +1,51 @@
 import executable from './executable.js'
 
-import Git from "nodegit"
-import Debug from "debug"
+import Debug from 'debug'
 const debug = Debug('fixme-ninja')
+
+import Git from 'nodegit'
+import pEvent from 'promisify-event'
 
 export default async function ninja(path){
    let repo = await Git.Repository.open(path)
-   let HEAD = await repo.getHeadCommit()
-   debug(`HEAD: ${HEAD.id().toString()}`)
+   let commit = await repo.getHeadCommit()
+   debug(`HEAD: ${commit.id().toString()}`)
+
+   let tree = await commit.getTree()
+   let walker = tree.walk()
+   let entries = pEvent(walker, 'end')
+   walker.start()
+
+   await Promise.all((await entries).map(async entry => {
+
+      if (entry.isTree()) {
+         debug(`Tree: ${entry.path()}`)
+         let tree = await entry.getTree()
+
+      } else { // isBlob()
+         debug(`Blob: ${entry.path()}`)
+         let blob = await entry.getBlob()
+         console.log(entry.filename(), entry.sha(), blob.rawsize() + "b")
+         console.log(blob.toString().split("\n").slice(0, 10).join("\n"))
+         let indices = find_fixmes(blob.toString())
+         console.log(indices)
+
+      }
+   }))
+}
+
+// FIXME: Make this less fucking ugly.
+function find_fixmes(string){
+   const indices = []
+   let i
+
+   while (true) {
+      i = string.indexOf('FIXME:', i)
+      if (i === -1) break
+      indices.push(i)
+   }
+
+   return indices
 }
 
 export { executable }
