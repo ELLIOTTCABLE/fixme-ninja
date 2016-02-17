5ce261d8ff7139f6877d6e05510ee67311285b72
ELLIOTTCABLE <me@ell.io>
2016-02-17 12:52:25 -0600

    + starting git-related support for tests

--- Tests/fixme-ninja.tests.js
+++ Tests/fixme-ninja.tests.js
@@ -1,2 +1,50 @@
-import { git } from './support.js'
+import { should } from './support.js'
 
+import ninja from '../'
+
+describe("fixme-ninja", function(){
+   describe("ninja()", function(){
+      this.eachNeeds("git init")
+
+      it("exists", function(){
+         should.exist(ninja)
+      })
+
+      // FIXME: This needs to get a directory allocated, but without `git init` ...
+      // FIXME: This shouldn't pass.
+      it("throws in a directory with no repository", function(){
+         ninja('.')
+      }).cd()
+
+      it("doesn't throw on an empty repository", function(){
+         console.log(process.cwd())
+         ninja('.')
+      })
+      .needs()
+
+   })
+})
+
+//describe("Meta", function(){
+//   describe("`git` support-function", function(){
+//
+//      it("evaluates", function(){
+//         (_=>{ git('show') }).should.not.throw()
+//      })
+//
+//      it("returns a Promise", function(){
+//         git('show').should.be.an.instanceof(Promise)
+//      })
+//
+//      it("resolves on completion", function(){
+//         return git('show').should.be.fulfuilled
+//      })
+//
+//      it("rejects on failure", function(){
+//         return git('show', 'deadbeef').should.be.rejected
+//      })
+//
+//   })
+//})
+
+setTimeout(()=> run(), 0)
--- Tests/support.js
+++ Tests/support.js
@@ -1,14 +1,130 @@
 /* @flow */
-import spawn from 'child_process'
+import Debug from 'debug'
+const debug = Debug('fixme-ninja:tests')
+
+import pexec      from 'promise-exec'
+import path       from 'path'
+import fs         from 'fs'
+import mkdirp     from 'mkdirp'
+import Bluebird   from 'bluebird'
+import _          from 'lodash'
+const  mkdir    = Bluebird.promisify(mkdirp)
+
+import { Test, Suite } from 'mocha'
 
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 
-chai.should()
-chai.use(chaiAsPromised)
+const should  = chai.should()
+                chai.use(chaiAsPromised)
 
-function git(){
+declare function before() : any
+declare function after() : any
 
+
+const module = require('../package.json')
+    , working_dir = path.resolve(module.config.dirs.working)
+
+before(function(){
+   return mkdir(working_dir).then(function(){
+      debug("Changing to working dir: %s", working_dir)
+      process.chdir(working_dir)
+   })
+})
+
+// FIXME: ALL of the CDs are happening before any tests run. I'm an idiot.
+Test.prototype.cd = function cd(name:?string) : boolean {
+   this.slug = _.kebabCase(name || this.fullTitle())
+   const dir = path.resolve(working_dir, this.slug)
+   var  prev = ''
+
+
+   this.parent.beforeAll(function(){
+      return mkdir(dir).then(function(){
+         prev = process.cwd()
+
+         debug("Changing to test dir: %s", dir)
+         process.chdir(dir)
+      })
+   })
+   this.parent.afterAll(function(){
+      debug("Returning to dir: %s", prev)
+      process.chdir(prev)
+   })
+
+   var isDirectory
+   try {
+      isDirectory = fs.statSync(dir).isDirectory()
+   } catch (err) {
+      if (err.code !== 'ENOENT') throw err
+      return false
+   }
+
+   if (!isDirectory) throw new Error('y u is file doh,')
+   return true
 }
 
-export { git }
+
+//function git(...args:Array<string>): Promise {
+//   return new Promise((resolve, reject) => {
+//      spawn('git', args, {stdio: ['ignore', 'ignore', 'inherit']})
+//         .on('close', code => {
+//            if (code === 0) resolve()
+//            else reject(code)
+//         })
+//   })
+//}
+
+// This takes either a function, or an array of shell-commands. These are executed before any
+// `needs()` of a test within this Suite.
+//---
+// TODO: Support nested Suites.
+Suite.prototype.eachNeeds =
+function eachNeeds(first: (string | ()=>any), ...commands: Array<string>) : void {
+   if (null == this._eachNeeds)
+      this._eachNeeds = []
+
+   if (typeof first == 'string')
+      commands.unshift(first)
+   else
+      this._eachNeeds.unshift(first)
+
+   if (commands.length)
+      this._eachNeeds = this._eachNeeds.concat(commands.map(function(str){
+         const command = ()=> pexec(str)
+         command.command = str
+         return command
+      }))
+}
+
+/* Adds a beforeAll that runs the specified commands. */
+Test.prototype.needs =
+function needs(first: (string | ()=>any), ...commands: Array<string>){
+   const already_cached = this.cd()
+
+   if (!already_cached) {
+      debug("Scheduling re-creation for '%s'", this.title)
+      var needs = []
+      if (this.parent._eachNeeds)
+         needs = needs.concat(this.parent._eachNeeds)
+
+      if (typeof first == 'string')
+         commands.unshift(first)
+      else
+         needs.unshift(first)
+
+      if (commands.length)
+         needs = needs.concat(commands.map(function(str){
+            const command = ()=> pexec(str)
+            command.command = str
+            return command
+         }))
+
+      debug("'%s' needs: %o", this.title, needs)
+
+      needs.forEach(need =>
+         this.parent.beforeAll(`'${this.title}': ${need.command}`, need) )
+   }
+}
+
+export { should }
